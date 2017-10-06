ee85b2cd59543f02f138297b1c597cfafe23d5a4
Matthew Buckner <bucknermr@gmail.com>
2017-10-05 23:32:10 -0700

    Implement class Queue

---
+++ w1/w1d5/queue.rb
@@ -0,0 +1,18 @@
+class Queue
+  def initialize(queue = [])
+    @queue = queue
+  end
+
+  def enqueue(el)
+    @queue.unshift(el)
+  end
+
+  def dequeue
+    @queue.pop
+  end
+
+  def show
+    @queue
+  end
+
+end
