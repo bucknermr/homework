315f287b91fc52d23dea2734313c3f3e998860d0
Matthew Buckner <bucknermr@gmail.com>
2017-11-07 23:41:55 -0800

    Add ajax request for weather in w6d3

--- w6/w6d3/js/index.js
+++ w6/w6d3/js/index.js
@@ -1,5 +1,13 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+$.ajax({
+  type: 'GET',
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  success: (response) => {
+    console.log(response);
+  }
 
+});
 // Add another console log here, outside your AJAX request
+console.log("Completed!");
